48f4268247a6acfbcb85b819a65ac7b62f7567d9
AcerFeng <qinlanfeng@foxmail.com>
2017-11-27 15:18:04 +0800

    完成首页布局

--- QuarkWebViewController/QuarkWebViewController/AppDelegate.m
+++ QuarkWebViewController/QuarkWebViewController/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    [self.window makeKeyAndVisible];
+    
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    self.window.rootViewController = nav;
+    
     return YES;
 }
 
--- QuarkWebViewController/QuarkWebViewController/ViewController.m
+++ QuarkWebViewController/QuarkWebViewController/ViewController.m
@@ -7,23 +7,109 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+
+static const CGFloat kMargin = 12.0;
+static const CGFloat kButtonWidth = 250;
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UITextField *inputTextField;
+@property (nonatomic, strong) UIButton *uiwebViewButton;
+@property (nonatomic, strong) UIButton *wkwebViewButton;
+@property (nonatomic, strong) UIButton *sonicwebViewButton;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"首页";
+    [self.view addSubview:self.inputTextField];
+    [self.view addSubview:self.uiwebViewButton];
+    [self.view addSubview:self.wkwebViewButton];
+    [self.view addSubview:self.sonicwebViewButton];
+    [self LF_setupSubviews];
 }
 
+- (void)LF_setupSubviews {
+    [self.inputTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(kMargin);
+        make.right.equalTo(self.view).offset(-kMargin);
+        make.top.equalTo(self.view).offset(100);
+    }];
+    
+    [self.uiwebViewButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.inputTextField);
+        make.top.equalTo(self.inputTextField.mas_bottom).offset(kMargin);
+        make.width.equalTo(@(kButtonWidth));
+        make.height.equalTo(@44);
+    }];
+    
+    [self.wkwebViewButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.inputTextField);
+        make.top.equalTo(self.uiwebViewButton.mas_bottom).offset(kMargin);
+        make.width.equalTo(@(kButtonWidth));
+        make.height.equalTo(@44);
+    }];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self.sonicwebViewButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.inputTextField);
+        make.top.equalTo(self.wkwebViewButton.mas_bottom).offset(kMargin);
+        make.width.equalTo(@(kButtonWidth));
+        make.height.equalTo(@44);
+    }];
 }
 
+#pragma mark - event response
+- (void)toUIWebView:(UIButton *)button {
+    
+}
+- (void)toWKWebView:(UIButton *)button {
+    
+}
+- (void)toSonikWebView:(UIButton *)button {
+    
+}
+
+#pragma mark - getters and setters
+- (UITextField *)inputTextField {
+    if (!_inputTextField) {
+        _inputTextField = [[UITextField alloc] init];
+        _inputTextField.backgroundColor = [UIColor greenColor];
+    }
+    return _inputTextField;
+}
+
+- (UIButton *)uiwebViewButton {
+    if (!_uiwebViewButton) {
+        _uiwebViewButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_uiwebViewButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_uiwebViewButton setTitle:@"使用uiwebview打开" forState:UIControlStateNormal];
+        [_uiwebViewButton addTarget:self action:@selector(toUIWebView:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _uiwebViewButton;
+}
+
+- (UIButton *)wkwebViewButton {
+    if (!_wkwebViewButton) {
+        _wkwebViewButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_wkwebViewButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_wkwebViewButton setTitle:@"使用wkwebview打开" forState:UIControlStateNormal];
+        [_wkwebViewButton addTarget:self action:@selector(toWKWebView:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _wkwebViewButton;
+}
+
+- (UIButton *)sonicwebViewButton {
+    if (!_sonicwebViewButton) {
+        _sonicwebViewButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_sonicwebViewButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_sonicwebViewButton setTitle:@"使用sonicwebview打开" forState:UIControlStateNormal];
+        [_sonicwebViewButton addTarget:self action:@selector(toSonikWebView:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _sonicwebViewButton;
+}
 
 @end
